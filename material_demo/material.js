cd869d69f2cd15c032a53d4b7bf603aefff95bad
Michal Mocny <mmocny@gmail.com>
2022-05-08 21:23:17 -0400

    Clean up material demo

--- material_demo/material.js
+++ material_demo/material.js
@@ -12,60 +12,6 @@ function attachMaterial() {
 	inpProgress = new mdc.linearProgress.MDCLinearProgress(document.querySelector('#inp-score'));
 }
 
-function registerHandlers() {
-	const fab = document.querySelector('.mdc-fab');
-	fab.addEventListener('click', (event) => {
-		stopAddingDelay();
-		setDelay(200);
-	});
-}
-
-
-let interval;
-function startAddingDelay() {
-	const MIN_DELAY = 0;
-	const MAX_DELAY = 500;
-	const RAMP_DELAY = 5000;
-	const RAMP_TIME = 30000;
-
-	// interval = setInterval(() => {
-	// 	const progress = (performance.now() - RAMP_DELAY) / RAMP_TIME;
-	// 	// Clamp progress to [0,1];
-	// 	setDelay(MIN_DELAY + (MAX_DELAY - MIN_DELAY) * delay_ratio);
-
-
-	// 	if (delay_ratio == 1) {
-	// 		stopAddingDelay();
-	// 	}
-	// }, 100);
-
-	const interval = setInterval(() => {
-		const progress = (performance.now() - RAMP_DELAY) / RAMP_TIME;
-		// Clamp progress to [0,1];
-		const delay_ratio = Math.max(0, Math.min(1, progress));
-		delayAmount = MIN_DELAY + (MAX_DELAY - MIN_DELAY) * delay_ratio;
-		// console.log('delay:', delayAmount);
-
-		delayProgress.foundation.setProgress(delay_ratio);
-
-		if (delay_ratio == 1) {
-			clearInterval(interval);
-		}
-	}, 100);
-}
-function stopAddingDelay() {
-	clearInterval(interval);
-}
-// function setDelay(ms) {
-// 	const delay_ratio = Math.max(0, Math.min(1, progress));
-// 	const progress = (performance.now() - RAMP_DELAY) / RAMP_TIME;
-// 	const delay_ratio = Math.max(0, Math.min(1, progress));
-
-// 	delayAmount = ms;
-// 	// console.log('delay:', delayAmount);
-// 	delayProgress.foundation.setProgress(delay_ratio);
-// }
-
 function startMeasuringInp() {
 	const GOOD = 200;
 	const BAD = 500;
@@ -81,16 +27,10 @@ function startMeasuringInp() {
 	}, true);
 }
 
-function startMeasuringMainThreadDelayOnOffscreenCanvas() {
-
-}
-
 function main() {
 	attachMaterial();
-	registerHandlers();
 	addResponsivenessJank();
 	startMeasuringInp();
-	startMeasuringMainThreadDelayOnOffscreenCanvas();
 }
 
 if (document.readyState === "complete" || document.readyState === "loaded" || document.readyState === "interactive") {
